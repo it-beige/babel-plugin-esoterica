4d8ae4ddb1bba7f1194ce7efda40079aac579174
beige <it_beige@163.com>
2023-03-06 12:41:45 +0800

    通过acorn extend方式编写add-keyword插件解析自定义的statement

--- acorn/src/plugins/add-keyword.js
+++ acorn/src/plugins/add-keyword.js
@@ -5,16 +5,35 @@ module.exports = function (options) {
 };
 
 function plugin(options, Parser) {
+  options = options || {};
+  const { word } = options;
   return class AddKeyword extends Parser {
     parse(program) {
       var newKeywords =
         'break case catch continue debugger default do else finally for function if return switch throw try var while with null true false instanceof typeof void delete new in this const class extends export import super';
-      newKeywords += ' guang'; // 增加一个关键字
+      newKeywords += ` ${word}`; // 增加一个关键字
       this.keywords = new RegExp(
         '^(?:' + newKeywords.replace(/ /g, '|') + ')$',
       );
 
       return super.parse(program);
+    }
+
+    parseStatement(context, topLevel, exports) {
+      let tokenType = this.type;
+
+      if (tokenType == Parser.acorn.keywordTypes[word]) {
+        let node = this.startNode();
+        return this.parseWordStatement(node, word);
+      } else {
+        return super.parseStatement(context, topLevel, exports);
+      }
+    }
+
+    parseWordStatement(node, word) {
+      this.next();
+      const upperCaseWord = word.slice(0, 1).toUpperCase() + word.slice(1);
+      return this.finishNode({ value: word }, `${upperCaseWord}Statement`);
     }
   };
 }
--- acorn/src/version1.js
+++ acorn/src/version1.js
@@ -1,3 +1,23 @@
-const { Parser } = require('acorn');
+const { Parser, TokenType } = require('acorn');
+const addKeyword = require('./plugins/add-keyword');
 
-const MyParser = Parser.extend(require('acorn-jsx')(), require('acorn-bigint'));
+const word = 'beige';
+Parser.acorn.keywordTypes[word] = new TokenType(word, {
+  keyword: word,
+});
+
+function wordsRegexp(words) {
+  return new RegExp('^(?:' + words.replace(/ /g, '|') + ')$');
+}
+
+const beigeKeyword = addKeyword({
+  word: word,
+});
+const newParser = Parser.extend(beigeKeyword);
+const program = `
+  beige
+  const a = 1
+`;
+
+const ast = newParser.parse(program);
+console.log(ast);

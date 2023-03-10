2279940e3d26a08bae721a26f9ce7b270131c888
beige <it_beige@163.com>
2023-03-10 23:57:06 +0800

    update: 便写auto-i18n-plugin国际化相关逻辑

--- exercize-auto-i18n/src/plugin/auto-i18n-plugin.js
+++ exercize-auto-i18n/src/plugin/auto-i18n-plugin.js
@@ -1,13 +1,32 @@
 const { declare } = require('@babel/helper-plugin-utils');
 
-declare((api, options, dirname) => {
+const autoI18nPlugin = declare((api, options, dirname) => {
+  api.assertVersion(7);
   return {
     pre(file) {},
 
     visitor: {
-      Program: {},
+      Program: {
+        enter(path, state) {
+          let imported;
+          path.traverse({
+            ImportDeclaration(path) {
+              let source = path.node.source.value;
+              if (source === 'intl') {
+                imported = true;
+              }
+            },
+          });
+
+          if (!imported) {
+            path.scope.generateUid('intl');
+          }
+        },
+      },
     },
 
     post(file) {},
   };
 });
+
+module.exports = autoI18nPlugin;
